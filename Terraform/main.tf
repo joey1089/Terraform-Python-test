033c32eda3ae5e7e9d47f858cf212ef1cae664f4
Joseph Peter <josephvpeter7@gmail.com>
2023-05-31 23:53:26 -0400

    added S3 public bucket policy in terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -18,3 +18,19 @@ resource "aws_s3_bucket" "demo_bucket" {
   count = length(var.bucket_name)
   bucket = var.bucket_name[count.index]
 }
+
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "terraform-public-jps3-01"
+  
+}
+
+# Set S3 policy to public
+resource "aws_s3_bucket_public_access_block" "s3-public-set" {
+  # count = length(var.bucket_name)
+  bucket = aws_s3_bucket.public_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
